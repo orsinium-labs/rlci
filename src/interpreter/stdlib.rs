453bf9064e79a8f02b7d5cd848fa328b29a422ed
gram <git@orsinium.dev>
2023-05-25 14:10:33 +0200

    test boolean logic

--- src/interpreter/stdlib.rs
+++ src/interpreter/stdlib.rs
@@ -15,3 +15,46 @@ pub fn read_stdlib() -> anyhow::Result<Vec<Module>> {
     }
     Ok(modules)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::interpreter::*;
+    use crate::parse;
+    use rstest::rstest;
+
+    const T: &str = "λa λb a";
+    const F: &str = "λa λb b";
+
+    #[rstest]
+    #[case::id(r#"id"#, "λa a")]
+    // test bool
+    #[case::true_repr(r#"true"#, T)]
+    #[case::false_repr(r#"false"#, F)]
+    #[case::not(r#"not true"#, F)]
+    #[case::not(r#"not false"#, T)]
+    #[case::and(r#"and false false"#, F)]
+    #[case::and(r#"and false true"#, F)]
+    #[case::and(r#"and true false"#, F)]
+    #[case::and(r#"and true true"#, T)]
+    #[case::or(r#"or false false"#, F)]
+    #[case::or(r#"or false true"#, T)]
+    #[case::or(r#"or true false"#, T)]
+    #[case::or(r#"or true true"#, T)]
+    #[case::xor(r#"xor false false"#, F)]
+    #[case::xor(r#"xor false true"#, T)]
+    #[case::xor(r#"xor true false"#, T)]
+    #[case::xor(r#"xor true true"#, F)]
+    #[case::xnor(r#"xnor false false"#, T)]
+    #[case::xnor(r#"xnor false true"#, F)]
+    #[case::xnor(r#"xnor true false"#, F)]
+    #[case::xnor(r#"xnor true true"#, T)]
+    fn stdlib(#[case] input: &str, #[case] exp: &str) {
+        let hinter = AutoCompleter::new();
+        let mut session = Session::new(&hinter);
+        session.load_stdlib().unwrap();
+        let module = parse(input).unwrap();
+        let val = session.eval_module(&module).unwrap();
+        println!("Input: {input}");
+        assert_eq!(val.repr(), exp);
+    }
+}
